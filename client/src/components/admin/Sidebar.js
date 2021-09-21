1bfa635032717146a69276603925c61d5344aeb0
omp2020 <om4981@gmail.com>
2021-09-21 17:29:22 +0530

    Login fixed & employee route added

--- client/src/components/admin/Sidebar.js
+++ client/src/components/admin/Sidebar.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Link } from "react-router-dom"
 import "../../css/sidebar.css"
 
-const Sidebar = () => {
+const Sidebar = ({ isAdmin }) => {
   return (
     <>
       <nav
@@ -11,44 +11,43 @@ const Sidebar = () => {
       >
         <div className="sidebar-sticky">
           {/* <img src={Logo} alt="Logo" style={{ width: "50px" }} /> */}
-          <ul className="nav flex-column">
-            {/* <li className="nav nav-item">
-              <Link to="/admin">
-                <SidebarComponent text="Admin Dashboard" />
-              </Link>
-            </li> */}
-            <li className="nav nav-item">
-              <Link to="/admin/createUser">
-                <SidebarComponent text="Add Employee" />
-              </Link>
-            </li>
-            <li className="nav nav-item">
-              <Link to="/admin/addDept">
-                <SidebarComponent text="Add Department" />
-              </Link>
-            </li>
-            <li className="nav nav-item">
-              <Link to="/admin/members">
-                <SidebarComponent text="Members" />
-              </Link>
-            </li>
-            <li className="nav nav-item">
-              <Link to="/admin/accounts">
-                <SidebarComponent text="Accounts" />
-              </Link>
-            </li>
-            {/* <li className="nav nav-item">
-              <SidebarComponent text="Payment" />
-            </li> */}
-            <li className="nav nav-item">
-              <Link to="/admin/services">
-                <SidebarComponent text="Services" />
-              </Link>
-            </li>
-            {/* <li className="nav nav-item">
-              <SidebarComponent text="Complaints" />
-            </li> */}
-          </ul>
+          {isAdmin == "true" ? (
+            <ul className="nav flex-column">
+              <li className="nav nav-item">
+                <Link to="/admin/createUser">
+                  <SidebarComponent text="Add Employee" />
+                </Link>
+              </li>
+              <li className="nav nav-item">
+                <Link to="/admin/addDept">
+                  <SidebarComponent text="Add Department" />
+                </Link>
+              </li>
+              <li className="nav nav-item">
+                <Link to="/admin/members">
+                  <SidebarComponent text="Members" />
+                </Link>
+              </li>
+              <li className="nav nav-item">
+                <Link to="/admin/accounts">
+                  <SidebarComponent text="Accounts" />
+                </Link>
+              </li>
+              <li className="nav nav-item">
+                <Link to="/admin/services">
+                  <SidebarComponent text="Services" />
+                </Link>
+              </li>
+            </ul>
+          ) : (
+            <ul className="nav flex-column">
+              <li className="nav nav-item">
+                <Link to="/employee/Route">
+                  <SidebarComponent text="Services" />
+                </Link>
+              </li>
+            </ul>
+          )}
         </div>
       </nav>
     </>
